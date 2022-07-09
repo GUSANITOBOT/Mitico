a49a9627227d26bb28d44e0605f0e13519376949
BotSatMagic <104804245+BotSatMagic@users.noreply.github.com>
2022-07-09 17:39:06 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,3 +1,4 @@
+/*
 let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
@@ -35,3 +36,4 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
 export default handler
+*/
